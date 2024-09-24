c1852fbe933216658d7ebbe55c84e55079bbf711
Reets123 <ibrahimalatig@gmail.com>
2024-09-24 13:49:24 +0000

    commit 2

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+// Lab 9: Github Cloning
+// COMSC-210 - Ibrahim Alatig
+
 #include <iostream>
 using namespace std;
 
@@ -5,7 +8,7 @@ int main() {
     // complete this code in order to make it output
     // the numbers 1-100
 
-    for ()
+    for (int i = 1; i <= 100; i++)
         cout << i << " ";
 
     return 0;
